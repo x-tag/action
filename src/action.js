248de649d0b2cc25f4afb9f8c5c1bcc0956be22b
Daniel Buchner <danieljb2@gmail.com>
2014-05-22 11:50:26 -0700

    Release 0.0.3

--- src/action.js
+++ src/action.js
@@ -3,11 +3,11 @@
   function executeTarget(e){
     console.log(e)
     var targets = xtag.query(document, this.target);
-    var action = this.action;
+    var method = this.method;
     var event = this.event;
     (targets[0] ? targets : [this]).forEach(function(target){
-      if (typeof target[action] === 'function'){
-        target[action]();
+      if (typeof target[method] === 'function'){
+        target[method]();
       }
       if (event) xtag.fireEvent(target, event);
     });
@@ -19,7 +19,7 @@
     },
     accessors: {
       target: { attribute: {}},
-      action: { attribute: {}},
+      method: { attribute: {}},
       event: { attribute: {}}
     },
     methods: {
