b325f2c6c0bd32c6659fd027eb36f2d4db4ad51d
Daniel Buchner <danieljb2@gmail.com>
2015-05-22 22:58:29 -0700

    updating action element js, removing stylus file

--- src/action.js
+++ src/action.js
@@ -1,14 +1,17 @@
 (function(){
 
   function executeTarget(e){
-    var targets = xtag.query(document, this.target);
-    var method = this.method;
-    var event = this.event;
-    (targets[0] ? targets : [this]).forEach(function(target){
-      if (typeof target[method] === 'function'){
-        target[method]();
-      }
-      if (event) xtag.fireEvent(target, event);
+    var node = this;
+    var targets = xtag.query(document, node.target);
+    var method = node.method;
+    var event = node.event;
+    (targets[0] ? targets : [node]).forEach(function(target){
+      if (target[method]) target[method]();
+      if (event) xtag.fireEvent(target, event, {
+        detail: {
+          actionElement: node
+        }
+      });
     });
   }
 
